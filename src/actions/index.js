24b3ce3aa9ea8ecb1c85473e19e2b5032cb3dc4b
michaeldai1006 <dtc74110@gmail.com>
2019-03-30 15:52:38 -0700

    Making a Request From an Action Creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,8 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+    const response = await jsonPlaceholder.get('/posts');
+
     return {
         type: 'FETCH_POSTS'
     };
